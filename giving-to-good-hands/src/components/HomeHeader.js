f99ae6731c4230d40f68d1d335c99fec1fd6301b
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-05-11 13:25:02 +0200

    base homepage construction

---
+++ giving-to-good-hands/src/components/HomeHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomeHeader() {
+  return (
+    <header>
+      {/* zawartosc */}
+    </header>
+  );
+}
+
+export default HomeHeader;
---
+++ giving-to-good-hands/src/components/HomeThreeColumns.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomeThreeColumns() {
+  return (
+    <header>
+      {/* zawar4tosc */}
+    </header>
+  );
+}
+
+export default HomeThreeColumns;
