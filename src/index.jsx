870bcb8ed584d284a7e32a396e19188bf3e5a50f
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-05-16 09:34:17 +0200

    fixing vite

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,11 @@
-import "./scss/_main.scss";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './scss/main.scss'
+import App from './App'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
